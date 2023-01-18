7a93be9c2223638a2724c6efa8e661f100a511f9
dzenanmecinovic <mecinovicd@gmail.com>
2023-01-18 18:02:47 +0100

    equality operators === vs ==

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -1,1 +1,26 @@
-elsese;
+// fundamentals - part 1
+// equality operators === vs ==
+
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+// ispituje i tip i vrednost
+if (age == 18) console.log("You just became an adult :D (loose)");
+// ispituje vrednost
+
+const favourite = Number(prompt("What's your favourite number?"));
+
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  // 23 === 23
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log(`7 is also a cool number!`);
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
