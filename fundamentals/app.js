f254dfebf60b0a8a53c657dc0af8d1092a6d42d7
dzenanmecinovic <mecinovicd@gmail.com>
2023-01-25 20:02:19 +0100

    Fundamentals2

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -228,37 +228,189 @@ TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106.
 /////// fundamentals - part 2
 /////// Functions calling other functions
 
-function cutFruitPieces(fruit) {
-  return fruit * 3;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 3;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
 
-console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor(2, 3));
 
 /////// fundamentals - part 2
 /////// Reviewing Functions
 
-const calcAge = function (birthYear) {
-  return 2023 - birthYear;
-};
+// const calcAge = function (birthYear) {
+//   return 2023 - birthYear;
+// };
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-  const age = calcAge(birthYear);
-  const retirement = 65 - age;
-  if (retirement > 0) {
-    return retirement;
-    console.log(`${firstName} retires in ${retirement} years`); // ignored ((return statements immediately exits the function))
-  } else {
-    return -1;
-    console.log(`${firstName} has already retired`); // ignored ((return statements immediately exits the function))
-  }
-};
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     return retirement;
+//     console.log(`${firstName} retires in ${retirement} years`); // ignored ((return statements immediately exits the function))
+//   } else {
+//     return -1;
+//     console.log(`${firstName} has already retired`); // ignored ((return statements immediately exits the function))
+//   }
+// };
 
-console.log(yearsUntilRetirement(2004, `Dzenan`));
-console.log(yearsUntilRetirement(1930, `Mike`));
+// console.log(yearsUntilRetirement(2004, `Dzenan`));
+// console.log(yearsUntilRetirement(1930, `Mike`));
+
+/////// fundamentals - part 2
+/////// coding challenge - functions
+
+// 2 gymnastics teams, dolphins and koalas
+
+// // test 1
+// const calcAverage = (first, second, third) => (first + second + third) / 3;
+// let scoreDolphin = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins won. (${avgDolphins} vs ${avgKoalas}.)`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas won. (${avgKoalas} vs ${avgDolphins})`);
+//   } else {
+//     console.log(`No team wins!`);
+//   }
+// };
+
+// checkWinner(scoreDolphin, scoreKoalas);
+
+// // test 2
+
+// scoreDolphin = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphin, scoreKoalas);
+// checkWinner(scoreDolphin, scoreKoalas);
+
+/////// fundamentals - part 2
+/////// basic array operations (methods)
+
+// const friends = ["Michael", "Steven", "Peter"];
+
+// // Add elements
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift(`John`);
+// console.log(friends);
+
+// // Remove elements
+// friends.pop(); // Last
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift(); // First
+// console.log(friends);
+
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.indexOf(`Bob`));
+
+// friends.push(23);
+// console.log(friends.includes("Steven"));
+// console.log(friends.includes(`Bob`));
+// console.log(friends.includes("23")); // (false) - it is testing with strict equality
+
+// if (friends.includes("Steven")) {
+//   console.log(`You have a friend called Steven`);
+// } else {
+//   console.log(`Negative.`);
+// }
+
+/////// fundamentals - part 2
+/////// coding challenge - arrays (#2)
+
+/*
+  Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+  1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on he rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+  2. And now let's use arrays! So create an array 'bills' containing the test data below.
+  3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+  4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+  TEST DATA: 125, 555 and 44
+*/
+
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(total);
+
+/////// fundamentals - part 2
+/////// objects
+
+// const jackieArray = [
+//   "Dzenan",
+//   "Mecinovic",
+//   2023 - 2004,
+//   "student",
+//   ["Prvi", "Drugi", "Treci"],
+// ];
+
+// console.log(jackieArray);
+
+// const jackie = {
+//   firstName: "Dzenan",
+//   lastName: "Mecinovic",
+//   age: 2023 - 2004,
+//   job: "student",
+//   friends: ["Prvi", "Drugi", "Treci"],
+// };
+// console.log(jackie);
+
+/////// fundamentals - part 2
+/////// DOT vs. BRACKET NOTATION
+
+const jackie = {
+  firstName: "Dzenan",
+  lastName: "Mecinovic",
+  age: 2023 - 2004,
+  job: "student",
+  friends: ["Prvi", "Drugi", "Treci"],
+};
+console.log(jackie);
+
+console.log(jackie.lastName);
+console.log(jackie["lastName"]);
+
+const nameKey = "Name";
+console.log(jackie["first" + nameKey]);
+console.log(jackie["last" + nameKey]);
+
+// console.log(jackie.'last' + nameKey); // doesn't work
+
+// const interestedIn = prompt(
+//   `What do you want to know about Jackie? Choose between firstName, lastName, age, job and friends.`
+// );
+
+// if (jackie[interestedIn]) {
+//   console.log(jackie[interestedIn]);
+// } else {
+//   console.log(
+//     `Wrong request! Choose between firstName, lastName, age, job and friends.`
+//   );
+// }
+
+jackie.location = "Serbia";
+jackie["twitter"] = "@dzenoooooo";
+console.log(jackie);
+
+// Challenge
+// "Jackie has 3 friends, and his best friend is called Prvi."
+console.log(
+  `${jackie.firstName} has ${jackie.friends.length} friends, and his best friend is called ${jackie.friends[0]}.`
+);
