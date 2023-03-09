35bfcd2e5bc4cac40d86c36048642dc1b002dbc1
dzenanmecinovic <mecinovicd@gmail.com>
2023-03-09 20:32:31 +0100

    looping arrays, object literals, optional chaining

--- 04 Data Structures, Modern Operators and Strings/app1.js
+++ 04 Data Structures, Modern Operators and Strings/app1.js
@@ -1,51 +1,102 @@
 "use strict";
 
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // open 24 Hours
+    close: 12 + 12,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // open 24 Hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  // ES6 enchanced object literals
+  openingHours,
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function (
-    starterIndex = 1,
-    mainIndex = 0,
-    time = "20:00",
-    address
-  ) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]}, and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.` // undefined??
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
     );
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
+
+/////////////////////////////////
+// Optional Chaining (?.)
+
+// WITHOUT optional chaining
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon?.open);
+
+// WITH optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? "closed";
+//   console.log(`On ${day}, we open at ${open}.`);
+// }
+
+// Methods
+// console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+// console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+// Arrays
+// const users = [{ name: "Dzeno", email: "mecinovic@gmail.com" }];
+
+// console.log(users[0]?.name ?? "User empty");
+// console.log(users[1]?.name ?? "User empty");
+
+// if (users.length > 0) console.log(users[0]?.name);
+// else console.log("User array empty");
+
+// if (restaurant.openingHours.fri) {
+//   console.log(restaurant.openingHours.fri.open);
+// }
+/////////////////////////////////
+// Enchanced Object Literals
+
+/////////////////////////////////
+// Looping Arrays: The for-of loop
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`);
+
+// console.log([...menu.entries()]);
+
 /////////////////////////////////
 // Local Assignment Operators
 // const rest1 = {
