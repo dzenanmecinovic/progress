092d3c95d736cb9266d076cf62d1271cc1d857ae
dzenanmecinovic <mecinovicd@gmail.com>
2023-03-12 15:52:59 +0100

    Maps Iteration

--- 04 Data Structures, Modern Operators and Strings/maps.js
+++ 04 Data Structures, Modern Operators and Strings/maps.js
@@ -45,39 +45,74 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+/////////////////////////////////////////////////
+// Maps Iteration
 
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again 😢"],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}.`);
+}
+// const answer = +prompt("Your answer");
+const answer = 3;
+console.log(answer);
+// if (answer === 3) console.log(question.get(true));
+console.log(question.get(question.get("correct") === answer)); // clg(quest.get(true));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+/////////////////////////////////////////////////
 // Maps
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// rest.set(2, "Lisbon, Portugal");
+// // console.log(rest);
+
+// rest
+//   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "We are open :D")
+//   .set(false, "We are closed :(");
 // console.log(rest);
 
-rest
-  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-  .set("open", 11)
-  .set("close", 23)
-  .set(true, "We are open :D")
-  .set(false, "We are closed :(");
-console.log(rest);
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// const time = 8;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
 
-const time = 8;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+// console.log(rest.has("categories"));
+// rest.delete(2);
+// // rest.clear();
+// console.log(rest);
+// console.log(rest.size);
 
-console.log(rest.has("categories"));
-rest.delete(2);
-// rest.clear();
-console.log(rest);
-console.log(rest.size);
+// const arr = [1, 2];
+// rest.set(arr, "Test");
+// console.log(rest);
+// console.log(rest.size);
 
-const arr = [1, 2];
-rest.set(arr, "Test");
-console.log(rest);
-console.log(rest.size);
-
-console.log(rest.get(arr)); // ako trazimo [1,2] vraca nam undefined, zato moramo niz da stavimo u promenljivu.
+// console.log(rest.get(arr)); // ako trazimo [1,2] vraca nam undefined, zato moramo niz da stavimo u promenljivu.
