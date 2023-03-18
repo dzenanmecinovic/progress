5981223c4e6baf8cfdab7e1d5f9337e5e377089f
dzenanmecinovic <mecinovicd@gmail.com>
2023-03-18 14:19:50 +0100

    strings #2

--- 04 Data Structures, Modern Operators and Strings/strings.js
+++ 04 Data Structures, Modern Operators and Strings/strings.js
@@ -1,40 +1,100 @@
 "use strict";
 const airline = "TAP Air Portugal";
-const plane = "A320";
 
-console.log(plane[0]);
-console.log(plane[1]);
-console.log(plane[2]);
-console.log("B737"[0]);
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
 
-console.log(airline.length);
-console.log("B737".length);
+// Fix capitalization in name
+const passenger = "dZeNaN"; // Dzenan
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
 
-console.log(airline.indexOf("r"));
-console.log(airline.lastIndexOf("r"));
-console.log(airline.indexOf("portugal"));
+function fixCapitalization(passengerName) {
+  const passLower = passengerName.toLowerCase();
+  const passCorrect = passLower[0].toUpperCase() + passLower.slice(1);
+  return passCorrect;
+}
 
-console.log(airline.slice(4));
-console.log(airline.slice(4, 7));
+// Comparing email
+const email = "hello@dzeno.io";
+const loginEmail = "     Hello@Dzeno.Io \n";
+const emailCorrect = loginEmail.toLowerCase().trim();
+console.log(emailCorrect);
+console.log(email === emailCorrect);
 
-console.log(airline.slice(0, airline.indexOf(" ")));
-console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+// Replacing
+const priceGB = "288,97€";
+const priceUS = priceGB.replace(",", ".").replace("€", "$");
+console.log(priceGB, priceUS);
 
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1)); // oduzima prvo i poslednje slovo
+const announcement = `All passengers come to barding door 23. Boarding door 23!`;
+console.log(announcement.replace("door", "gate"));
+console.log(announcement.replaceAll("door", "gate"));
 
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === "B" || s === "E") console.log(`Middle Seat ✈`);
-  else console.log(`Not a Middle Seat 😥`);
+console.log(announcement.replace(/door/g, "gate"));
+
+// Booleans
+const plane = "Airbus A320neo";
+console.log(plane.includes("A320"));
+console.log(plane.includes("Boeing"));
+console.log(plane.startsWith("Aibr"));
+
+if (plane.startsWith("Airbus") && plane.endsWith("neo"))
+  console.log("Part of the NEW Airbus family ✈");
+
+// Pratice exercises
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun"))
+    console.log("You are NOT allowed on board! 🚫");
+  else console.log("Welcome aboard! ✅");
 };
-checkMiddleSeat("11B");
-checkMiddleSeat("23C");
-checkMiddleSeat("3E");
+checkBaggage("I have a laptop, some Food and a pocket Knife");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
 
-console.log(new String("dzenan"));
-console.log(typeof new String("dzenan"));
+// console.log(fixCapitalization("dZENAN"));
+// console.log(fixCapitalization("necijeIme"));
+// console.log(fixCapitalization("IsMaIL"));
+// console.log(fixCapitalization("AMar"));
 
-console.log(new String("dzenan").slice(-3));
-console.log(new String("dzenan").slice(0, 3));
+//part 1
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log("B737"[0]);
+
+// console.log(airline.length);
+// console.log("B737".length);
+
+// console.log(airline.indexOf("r"));
+// console.log(airline.lastIndexOf("r"));
+// console.log(airline.indexOf("portugal"));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(" ")));
+// console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1)); // oduzima prvo i poslednje slovo
+
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === "B" || s === "E") console.log(`Middle Seat ✈`);
+//   else console.log(`Not a Middle Seat 😥`);
+// };
+// checkMiddleSeat("11B");
+// checkMiddleSeat("23C");
+// checkMiddleSeat("3E");
+
+// console.log(new String("dzenan"));
+// console.log(typeof new String("dzenan"));
+
+// console.log(new String("dzenan").slice(-3));
+// console.log(new String("dzenan").slice(0, 3));
